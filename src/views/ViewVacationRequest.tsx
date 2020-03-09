a169837759f6a0c4ac0eb8529d8e4d2e3d3b5d05
Helene Boge <helene.boge@no.experis.com>
2020-03-09 14:22:03 +0100

    Added som views and routes

---
+++ src/views/ViewVacationRequest.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ViewVacationRequest extends Component{
+    render() {
+        return <h1>View vacation request</h1>
+    }
+}
+
+export default ViewVacationRequest;
