da47d7a586cd1ed12faaa2990b8c4f1e80ab3e24
“Elisabeth <medlien0@hotmail.com>
2020-03-09 15:08:52 +0100

    new init with js

--- src/views/CreateVacationRequest.tsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class CreateVacationRequest extends Component{
-    render() {
-        return <h1>Create vacation request</h1>
-    }
-}
-
-export default CreateVacationRequest;
