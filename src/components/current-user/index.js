0cc4d80d80924e753c1637f52d500b0aeea7cff5
Helene Boge <helene.boge@no.experis.com>
2020-03-10 11:10:00 +0100

    added application frame with some content

---
+++ src/components/current-user/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class CurrentUser extends Component{
+    render(){
+        return(
+            <div>
+                <p>Currently logged in user: Username</p>
+            </div>
+        )
+    }
+}
+
+export default CurrentUser;
---
+++ src/components/notifications/index.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import {Link, NavLink} from 'react-router-dom';
+import { Dropdown, Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
+import {LinkContainer} from 'react-router-bootstrap';
+
+class Notifications extends Component{
+    render(){
+        return(
+            <div>
+                    <Dropdown>
+                        <Dropdown.Toggle variant="warning" id="dropdown-notifications">
+                            Notifications
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item>
+                                Notification 1
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Notification 2
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+        )
+    }
+}
+
+export default Notifications;
