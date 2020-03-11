3cfe05fffca43cb9ae3ee25a08256b31029c96fa
Helene Boge <helene.boge@no.experis.com>
2020-03-11 11:59:13 +0100

    added nicely looking navbar for both user and admin

--- src/components/application-frame/index.js
+++ src/components/application-frame/index.js
@@ -4,15 +4,57 @@ import {MDBBtn} from 'mdbreact';
 import AvailableActions from '../available-actions/index';
 import Notifications from '../notifications/index';
 import CurrentUser from '../current-user/index';
-import './ApplicationFrame.css'
+import './ApplicationFrame.css';
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import SettingsIcon from '@material-ui/icons/Settings';
+
 
 class ApplicationFrame extends Component{
+    
+
     render(){
+        const navDropdownTitle = ( <span><NotificationsIcon size="sm"/></span> );
+        const settingsTitle = (<span><SettingsIcon size="sm"/></span>);
+
         return(
             <div className="container">
-                <CurrentUser />
-                <Notifications />
-                <AvailableActions />
+                <div id="apFrameForUsers">
+                    <Navbar fixed="top" expand="md" bg="dark" variant="dark">
+                        <Navbar.Brand>Tidsbanken</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/"> Home</Nav.Link>
+                            <Nav.Link href="/VacationRequests">Vacation requests</Nav.Link>
+                            <NavDropdown title={navDropdownTitle}>
+
+                            </NavDropdown>
+                        </Nav>
+                        <Navbar.Collapse className="justify-content-end">
+                            <Navbar.Text> Signed in as: <Link to="/UserProfile">User name</Link></Navbar.Text>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+                {/* Elementet under vises ikke pga fixed="top" i div over*/}
+                <div id="apFrameForAdmin">
+                    <Navbar expand="md" bg="dark" variant="dark">
+                    <Navbar.Brand>Tidsbanken</Navbar.Brand>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/"> Home</Nav.Link>
+                        
+                        <NavDropdown title={navDropdownTitle}>
+
+                        </NavDropdown>
+                    </Nav>
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text> Signed in as: <Link to="/UserProfile">Admin name</Link></Navbar.Text>
+                        <Nav>
+                            <Nav.Link href="/ApplicationSettings"> <SettingsIcon/></Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Navbar>
+
+                </div>
+                
             </div>
         )
     }
