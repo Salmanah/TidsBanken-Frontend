e8cdc310f7bd8e1652d5b57b2984220159bb3496
Salman <salman.ahmed@no.experis.com>
2020-03-11 16:09:36 +0100

    Working on getting shit to work

--- src/views/login-page/index.js
+++ src/views/login-page/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {withCookies, Cookies, useCookies} from 'react-cookie';
-
+import { Link, NavLink } from 'react-router-dom';
+import './AppHeader.css';
 
 class LoginPage extends Component {
 
@@ -8,7 +9,39 @@ class LoginPage extends Component {
 
     render(){
 
-        return(<h1>Login page</h1>)
+        return(
+            <div>
+                <header className="app-header">
+                    <div className="container">
+                        <div className="app-branding">
+                            <Link to="/" className="app-title">Spring Social</Link>
+                        </div>
+                        <div className="app-options">
+                            <nav className="app-nav">
+                                { this.props.authenticated ? (
+                                    <ul>
+                                        <li>
+                                            <NavLink to="/profile">Profile</NavLink>
+                                        </li>
+                                        <li>
+                                            <a onClick={this.props.onLogout}>Logout</a>
+                                        </li>
+                                    </ul>
+                                ): (
+                                    <ul>
+                                        <li>
+                                            <NavLink to="/login">Login</NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink to="/signup">Signup</NavLink>
+                                        </li>
+                                    </ul>
+                                )}
+                            </nav>
+                        </div>
+                    </div>
+                </header>
+        )
     }
 }
 
