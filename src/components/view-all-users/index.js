fd4161168a1c20c99e0c6e7d46da383bfc182bde
Helene Boge <helene.boge@no.experis.com>
2020-03-12 14:04:04 +0100

    Worked on list of users in admin settings

--- src/components/view-all-users/index.js
+++ src/components/view-all-users/index.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import {List, ListItem} from '@material-ui/core';
+import UserListItem from '../../components/user-list-item/index';
+import { List, ListItem, Checkbox, ListItemIcon, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
 class ViewAllUsers extends Component{
 
+
+
+
     render(){
+
         return(
+
+
             <div>
                 <h4>(Searchable?) list of all users</h4>
                 <List>
-                    <ListItem> User 1 </ListItem>
-                    <ListItem> User 2 </ListItem>
+                    <UserListItem />
                 </List>
             </div>
         )
