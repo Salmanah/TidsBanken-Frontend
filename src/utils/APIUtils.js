9b376c24cb836ee521a4b3730839b5df59cb62f7
“Elisabeth <medlien0@hotmail.com>
2020-03-16 21:13:38 +0100

    repeated function removed

--- src/utils/APIUtils.js
+++ src/utils/APIUtils.js
@@ -4,30 +4,30 @@ const request = (options) => {
     const headers = new Headers({
         'Content-Type': 'application/json',
     })
-    
-    if(localStorage.getItem(ACCESS_TOKEN)) {
+
+    if (localStorage.getItem(ACCESS_TOKEN)) {
         headers.append('Authorization', 'Bearer ' + localStorage.getItem(ACCESS_TOKEN))
     }
 
-    const defaults = {headers: headers};
+    const defaults = { headers: headers };
     options = Object.assign({}, defaults, options);
 
     return fetch(options.url, options)
-    .then(response => 
-        response.json().then(json => {
-            if(!response.ok) {
-                return Promise.reject(json);
-            }
-            return json;
-        })
-    );
+        .then(response =>
+            response.json().then(json => {
+                if (!response.ok) {
+                    return Promise.reject(json);
+                }
+                return json;
+            })
+        );
 };
 
 export function getCurrentUser() {
-    if(!localStorage.getItem(ACCESS_TOKEN)) {
+    if (!localStorage.getItem(ACCESS_TOKEN)) {
         return Promise.reject("No access token set.");
     }
-    console.log("SENDING REQUEST TO BACKEND ",API_BASE_URL+"/user/1")
+    console.log("SENDING REQUEST TO BACKEND ", API_BASE_URL + "/user/1")
     return request({
         url: API_BASE_URL + "/user/1",
         method: 'GET'
@@ -35,29 +35,18 @@ export function getCurrentUser() {
 }
 
 export function getOtherUser(id) {
-    if(!localStorage.getItem(ACCESS_TOKEN)) {
+    if (!localStorage.getItem(ACCESS_TOKEN)) {
         return Promise.reject("No access token set.");
     }
-    console.log("SENDING REQUEST TO BACKEND ",API_BASE_URL+`/user/${id}`)
+    console.log("SENDING REQUEST TO BACKEND ", API_BASE_URL + `/user/${id}`)
     return request({
-        url: API_BASE_URL +`/user/${id}`,
-        method: 'GET'
-    });
-}
-
-export function getOtherUser(id) {
-    if(!localStorage.getItem(ACCESS_TOKEN)) {
-        return Promise.reject("No access token set.");
-    }
-    console.log("SENDING REQUEST TO BACKEND for OTHER USER ",API_BASE_URL+`/user/${id}`)
-    return request({
-        url: API_BASE_URL +`/user/${id}`,
+        url: API_BASE_URL + `/user/${id}`,
         method: 'GET'
     });
 }
 
 export function login(loginRequest) {
-    console.log("HERE IS THE REQUEST ",loginRequest)
+    console.log("HERE IS THE REQUEST ", loginRequest)
     return request({
         url: API_BASE_URL + "/auth/login",
         method: 'POST',
