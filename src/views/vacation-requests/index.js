9d53eb5a569cc04f1b62026636522f84de7ad1fa
Helene Boge <helene.boge@no.experis.com>
2020-03-18 15:31:41 +0100

    fixed vacation request props

--- src/views/vacation-requests/index.js
+++ src/views/vacation-requests/index.js
@@ -3,18 +3,18 @@ import './vacationRequests.css';
 import { List, Divider } from '@material-ui/core';
 import RequestListItem from '../../components/requestListItem/index';
 
-const VacationRequests = () => {
+const VacationRequests = (props) => {
 
     return (
         <div>
             <div className="vacationRequestContent">
-                <h3>This is the request list of user with id: {this.props.currentUser.id}</h3>
-                <h4>Name: {this.props.currentUser.name}</h4>
+                <h3>This is the request list of user with id: {props.currentUser.id}</h3>
+                <h4>Name: {props.currentUser.name}</h4>
                 <div>
                     <List>
-                        <RequestListItem title="Request 1" parentProps={this.props} />
+                        <RequestListItem title="Request 1" parentProps={props} />
                         <Divider />
-                        <RequestListItem title="Request 2" parentProps={this.props} />
+                        <RequestListItem title="Request 2" parentProps={props} />
                         <Divider />
                     </List>
                     {/*
