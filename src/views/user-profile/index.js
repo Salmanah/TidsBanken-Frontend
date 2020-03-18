8d7ddd1c48e8139d7a3ca6740a25ef5375c41668
Helene Boge <helene.boge@no.experis.com>
2020-03-18 10:29:15 +0100

    Fixed a bit on user profile

--- src/views/user-profile/index.js
+++ src/views/user-profile/index.js
@@ -3,11 +3,15 @@ import ApplicationFrame from '../../components/application-frame/index';
 import EditIcon from '@material-ui/icons/Edit';
 import EditRoundedIcon from '@material-ui/icons/EditRounded';
 import {Button} from 'react-bootstrap';
-import {List, ListItem, ListItemIcon, ListItemText, IconButton} from '@material-ui/core';
+import {List, ListItem, ListItemIcon, ListItemText, IconButton, TextField} from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import MailIcon from '@material-ui/icons/Mail';
+import { getCurrentUser } from "../../utils/APIUtils";
+import CancelIcon from '@material-ui/icons/Cancel';
+import CloseIcon from '@material-ui/icons/Close';
+import CheckIcon from '@material-ui/icons/Check';
 
-class UserProfile extends Component{
+class UserProfile extends Component {
 
     constructor(props){
         super(props)
@@ -15,15 +19,46 @@ class UserProfile extends Component{
             name : "No name",
             email : "No email",
             profilePic : "No picture",
-            isAdmin : false
+            isAdmin : false,
+            editEmail : false,
         }
     }
 
     editClick(event){
-        console.log("edit pushed");
+        console.log("edit email pushed");
+        //perform update of email
+        this.setState({
+            editEmail : true
+        })
+    }
+
+    emailInputChange(event){
+        console.log(event.target.value);
+    }
+
+    //Brukes ikke per nå
+    componentDidMount(){
+        getCurrentUser().then(response => {
+            console.log(response)
+            this.setState({
+                name : response.name,
+                email : response.email,
+                profilePic : response.imageUrl,
+                isAdmin : response.admin
+            })
+        })
+    }
+
+    handleCancelEdit(event){
+        this.setState({
+            editEmail : false
+        })
     }
 
     render() {
+
+        console.log("editemail: " + this.state.editEmail)
+
         return(
             <div>
                 <h1>User profile</h1>
@@ -32,11 +67,24 @@ class UserProfile extends Component{
                         <ListItemIcon><PersonIcon/></ListItemIcon>
                         <ListItemText>{this.props.currentUser.name}</ListItemText>
                     </ListItem>
-                    <ListItem>
-                        <ListItemIcon><MailIcon/></ListItemIcon>
-                        <ListItemText>{this.props.currentUser.email}</ListItemText>
-                        <IconButton edge="end" aria-label="edit"><EditIcon/></IconButton>
-                    </ListItem>
+                    {this.state.editEmail ? (
+                        <div>
+                            <ListItem>
+                                <ListItemIcon><MailIcon/></ListItemIcon>
+                                <TextField placeholder="Enter email here" onChange={e => this.emailInputChange(e)}></TextField>
+                                <IconButton edge="end" aria-label="cancel" onClick={e => this.handleCancelEdit(e)}><CloseIcon/></IconButton>
+                                <IconButton edge="end" aria-label="approve"><CheckIcon/></IconButton>
+                            </ListItem>
+                        </div>
+                    ) : (
+                        <div>
+                            <ListItem>
+                                <ListItemIcon><MailIcon/></ListItemIcon>
+                                <ListItemText>{this.props.currentUser.email}</ListItemText>
+                                <IconButton edge="end" aria-label="edit" onClick={e => this.editClick(e)}><EditIcon/></IconButton>
+                            </ListItem>
+                        </div>
+                    )}
                 </List>
                 <img src={this.props.currentUser.imageUrl} alt="No image"/><Button variant="outline-secondary" size="sm" onClick={e => this.editClick(e)}><EditIcon /></Button>
             </div>
