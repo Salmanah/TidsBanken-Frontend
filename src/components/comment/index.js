046a1f84c889dbb4de892a1aa08f253340817b8e
Helene Boge <helene.boge@no.experis.com>
2020-03-23 12:16:12 +0100

    comment formatting

--- src/components/comment/index.js
+++ src/components/comment/index.js
@@ -1,12 +1,31 @@
 import React, {useEffect, useState} from 'react';
-import {ListItem, ListItemText} from '@material-ui/core';
+import {ListItem, ListItemText, Divider} from '@material-ui/core';
+import {Row, Col} from 'react-bootstrap';
+import { Colorize } from '@material-ui/icons';
+import './comment.css';
 
 const Comment = (props) => {
 
+    console.log("Comment component")
+    console.log(props.comment)
+
     return(
-        <ListItem>
-            <ListItemText>{props.comment.message}</ListItemText>
-        </ListItem>
+        <div>
+            <ListItem>
+                <ListItemText className="itemAboutComment">
+                    <Row>
+                        <Col>By: {props.comment.user[0].name}</Col>
+                        <Col className="commentTimestamp">{props.comment.datetimestamp}</Col>
+                    </Row> 
+                </ListItemText>
+            </ListItem>
+            <ListItem>
+                <ListItemText>
+                    {props.comment.message}
+                </ListItemText>
+            </ListItem>
+        </div>
+        
     )
 }
 
