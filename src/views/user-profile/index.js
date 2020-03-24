17aa2344bf891d75a0c882267ed3dc726d1bcaa3
Helene Boge <helene.boge@no.experis.com>
2020-03-24 12:37:37 +0100

    changed layout

--- src/views/user-profile/index.js
+++ src/views/user-profile/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import EditIcon from '@material-ui/icons/Edit';
 //import EditRoundedIcon from '@material-ui/icons/EditRounded';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Col, Row } from 'react-bootstrap';
 import { List, ListItem, ListItemIcon, ListItemText, IconButton, TextField } from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import MailIcon from '@material-ui/icons/Mail';
@@ -10,6 +10,49 @@ import { getCurrentUser } from "../../utils/APIUtils";
 import CloseIcon from '@material-ui/icons/Close';
 import CheckIcon from '@material-ui/icons/Check';
 
+const UserProfile = () => {
+
+
+    const [user, setUser] = useState(Object);
+
+
+
+    useEffect(()=>{
+
+        getCurrentUser().then(response => {
+
+            setUser(response)
+        })
+
+    },[])
+
+
+    return(
+        <Container>
+
+            <Row>
+                <Col sm={3.5}>
+                    <img src={user.imageUrl} width="300" alt="Profile picture" />
+                </Col>
+                <Col>
+                    <List>
+                        <ListItem><ListItemText>Name: {user.name}</ListItemText></ListItem>
+                        <ListItem><ListItemText>Email: {user.email}</ListItemText></ListItem>
+                        {user.admin ? (
+                            <ListItem><ListItemText>Admin</ListItemText></ListItem>
+                        ) : (
+                            <ListItem><ListItemText>User</ListItemText></ListItem>
+                        )}
+                        
+                    </List>
+                </Col>
+            </Row>
+
+        </Container>
+    )
+}
+
+/*
 class UserProfile extends Component {
 
     constructor(props) {
@@ -86,5 +129,6 @@ class UserProfile extends Component {
         )
     }
 }
+*/
 
 export default UserProfile;
