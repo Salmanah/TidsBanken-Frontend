404b317d6640c8766d8fa10ffdc34fd606966367
Helene Boge <helene.boge@no.experis.com>
2020-03-30 12:29:02 +0200

    inline comments views/users

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import './users.css';
 import { getAllUsers } from '../../utils/APIUtils';
@@ -6,18 +6,22 @@ import {List} from '@material-ui/core';
 import UserListItem from '../../components/user-list-item/index';
 import {Spinner} from 'react-bootstrap';
 
+
+//Admin setting view that displays a list of all registered users
+//Each list item is rendered as a UserListItem component
 const Users = (props) => {
 
-    const [users, setUsers] = React.useState([])
-    const [loading, setLoading] = React.useState(true)
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(true)
 
+    //Fetching all users of the system
     useEffect(()=>{
         getAllUsers()
         .then(response=>{
             setUsers(response);
             setLoading(false);
         })
-        .catch(error => {console.error('Error:', error)})
+        .catch(error => {console.error(error)})
     }, [])
 
     if (props.currentUser.admin){
@@ -25,27 +29,24 @@ const Users = (props) => {
             <Container>
                <Row>
                    <Col md={{ span: 8, offset: 2 }}>
-                   <div>
                         <h1>User settings</h1>
-                        {loading ? 
-                        (<Spinner animation="border" />) 
-                        : (
+                    </Col>
+                </Row>
+                {loading ? 
+                    (<Row><Col md={{ span: 8, offset: 5 }}><Spinner animation="border" /></Col></Row>) 
+                    : (
+                        <Row>
+                        <Col md={{ span: 8, offset: 2 }}>
                             <List>
                                 {users.map((element, index) => {
-                                    
-                                        return <UserListItem key={index} user={element} parentProps={props} />
-                                    
+                                    return <UserListItem key={index} user={element} parentProps={props} />                                    
                                 })}
-                            </List>)
-                        }
-                    </div>
-                   </Col>
-               </Row>
-                    
-  
-                
+                            </List>
+                        </Col>
+                        </Row>
+                        )
+                    }       
             </Container>
-        
     )
     } else {
         return(
