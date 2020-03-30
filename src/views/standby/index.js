917a46889937c81ce078ec73063d859d55a6f0ea
Helene Boge <helene.boge@no.experis.com>
2020-03-30 16:55:43 +0200

    userprofile, myprofile, standby fixed

--- src/views/standby/index.js
+++ src/views/standby/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './standby.css';
 
 const Standby = () => {
-    return(<p>Waiting for user to be verified by an administrator</p>)
+    return(
+            <div className="standbyText">
+                <h1>Tidsbanken vacation planner</h1>
+                <p>Ooops.. it seems your e-mail have not been verified by an administrator yet.</p>
+                <p>Please wait or contact your administrator to proceed.</p>
+            </div>
+        )
 }
 
 export default Standby;
